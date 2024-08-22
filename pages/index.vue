26a1f8b429e8a6af5bb2130f2d90e3b11ca4cab2
sharonBenish <sbenishwilliams@gmail.com>
2024-08-21 19:07:18 -0500

    fix: stle fixes

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ async function submitForm() {
 
 <template>
     <!--hero section-->
-    <div class="relative wfull bg-[url('/images/hero.jpeg')] bg-cover bg-center h50dvh md:h65dvh">
+    <div class="relative wfull bg-[url('/images/hero.jpeg')] bg-cover bg-center h50dvh md:h70dvh">
         <div class="w-site px-0">
             <div class="absolute lt-sm:top-8 lt-sm:left-1/2 lt-sm:-translate-x-[50%] z-20 px-10 xl:px-0 hfull wfull">
                 <div class="sm:relative bg-e-primary/70 rounded-4 text-6 sm:text-8 lg:text-11 fw800 text-white w-full sm:max-w-150 px6 py6 top-15 lt-sm:text-center">Turn Parking Spots Into Paid Rent</div>
@@ -44,7 +44,7 @@ async function submitForm() {
             </div>
         </div>
 
-        <div class="overlay absolute top-0 hfull wfull bg-[#D9D9D9] opacity-40"></div>
+        <div class="overlay absolute top-0 hfull wfull bg-[#D9D9D9] opacity-20"></div>
     </div>
 
     <!--About us Section-->
@@ -54,26 +54,26 @@ async function submitForm() {
             <div class="h2 w10 bg-e-secondary"></div>
         </div>
 
-        <div class="flex flex-col md:flex-row gap-10 md:gap-5 lg:gap-8 flex-row justify-between my8">
+        <div class="flex flex-col md:flex-row gap-10 md:gap-5 lg:gap-8 flex-row justify-between my8 fw600">
             <div class="wfull md:w-1/3 h70 sm:h80 lg:h95">
-                <img src="/images/corner.jpeg" class="h-[70%] md:h-[65%] lg:h-[70%] wfull object-cover" />
-                <div class="bg-e-primary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[30%] md:h-[35%] lg:h-[30%] rounded-b-4 text-center">
-                    <h4 class="text-3.5 lg:text-4 fw600 mb2">In Tiny Corners</h4>
-                    <p class="text-3.2 lg:text-3.5">Compact car only parking space in tiny corners are not a problem</p>
+                <img src="/images/corner.jpeg" class="h-[65%] lg:h-[70%] wfull object-cover" />
+                <div class="bg-e-primary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[35%] lg:h-[30%] rounded-b-4 text-center">
+                    <h4 class="text-4 fw600 mb1">In Tiny Corners</h4>
+                    <p class="text-3.5">Compact car only parking space in tiny corners are not a problem</p>
                 </div>
             </div>
             <div class="wfull md:w-1/3 h70 sm:h80 lg:h95">
-                <img src="/images/backyard.jpeg" class="h-[70%] md:h-[65%] lg:h-[70%] wfull object-cover" />
-                <div class="bg-e-secondary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[30%] md:h-[35%] lg:h-[30%] rounded-b-4 text-center">
-                    <h4 class="text-3.5 lg:text-4 fw600 mb2">In The Backyard</h4>
-                    <p class="text-3.2 lg:text-3.5">Parking spot out back or a big lot, backyard parking is a specialty</p>
+                <img src="/images/backyard.webp" class="h-[65%] lg:h-[70%] wfull object-cover" />
+                <div class="bg-e-secondary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[35%] lg:h-[30%] rounded-b-4 text-center">
+                    <h4 class="text-4 fw600 mb1">In The Backyard</h4>
+                    <p class="text-3.5">Parking spot out back or a big lot, backyard parking is a specialty</p>
                 </div>
             </div>
             <div class="wfull md:w-1/3 h70 sm:h80 lg:h95">
-                <img src="/images/alley.jpeg" class="h-[70%] md:h-[65%] lg:h-[70%] wfull object-cover"  />
-                <div class="bg-e-primary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[30%] md:h-[35%] lg:h-[30%] rounded-b-4 text-center">
-                    <h4 class="text-3.5 lg:text-4 fw600 mb2">In The Alley</h4>
-                    <p class="text-3.2 lg:text-3.5">Narrow allies aren’t scary to us; we thrive back here</p>
+                <img src="/images/alley.webp" class="h-[65%] lg:h-[70%] wfull object-cover"  />
+                <div class="bg-e-primary/80 flex flex-col justify-center text-white px8 md:px4 lg:px8 h-[35%] lg:h-[30%] rounded-b-4 text-center">
+                    <h4 class="text-4 fw600 mb1">In The Alley</h4>
+                    <p class="text-3.5">Narrow allies aren’t scary to us; we thrive back here</p>
                 </div>
             </div>
         </div>
@@ -89,34 +89,34 @@ async function submitForm() {
             </div>
 
             <div class="grid grid-cols-2 md:grid-cols-3 gap-5 sm:gap10 lg:gap30 mt10 lt-sm:px0 px6 md:px0">
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/worthy.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">Rent paid on time automatically</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/worthy.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 px-2 lg:px-4 fw600 bg-e-secondary text-white h-[35%] sm:h-[25%] flex items-center justify-center">Rent paid on time automatically</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">Credit worthy tenants</div>
                 </div>
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/premiums.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">In some markets, we pay above market rent!</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/premiums.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 px-2 lg:px-4 bg-e-secondary text-white fw600 h-[35%] sm:h-[25%] flex items-center justify-center">In some markets, we pay above market rent!</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">Rent premiums</div>
                 </div>
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/partners.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">SpotHero  works with us only in certain locations!</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/partners.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 px-2 lg:px-4 fw600 bg-e-secondary text-white h-[35%] sm:h-[25%] flex items-center justify-center">SpotHero works only with us in certain locations!</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">Exclusive partnership</div>
                 </div>
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/turn.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">Leases renewed year after year</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/turn.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 fw600 px-2 lg:px-4 bg-e-secondary text-white h-[35%] sm:h-[25%] flex items-center justify-center">Leases renewed year after year</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">Reduced Turnover</div>
                 </div>
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/no-vacancy.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">Fill your spots when the residential market can’t!</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/no-vacancy.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 px-2 lg:px-4 bg-e-secondary text-white fw600 h-[35%] sm:h-[25%] flex items-center justify-center">Fill your spots when the residential market can’t!</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">No vacancy</div>
                 </div>
-                <div class="h50 xs:h60 relative">
-                    <img src="/images/advice.webp" class="h-[65%] sm:h-[80%] w-full object-cover bg-center"/>
-                    <div class="lt-xs:text-3.5 px-2 bg-[#D9D9D9] text-e-primary h-[35%] sm:h-[20%] flex items-center justify-center">Great direction, photos, and signage solve problems</div>
+                <div class="h50 xs:h60 lg:h65 relative">
+                    <img src="/images/advice.webp" class="h-[65%] sm:h-[75%] w-full object-cover bg-center"/>
+                    <div class="lt-xs:text-3.5 px-2 lg:px-4 fw600 bg-e-secondary text-white h-[35%] sm:h-[25%] flex items-center justify-center">Great direction, photos, and signage solve problems</div>
                     <div data-aos="fade-right" class="absolute bottom-16 xs:bottom-20 -left-5 sm:-left-8 w-[150px] sm:w-max bg-e-primary lt-sm:text-3.5 h7 xs:h9 flex justify-center sm:px-8 items-center text-white rounded-tl-2.5 fw600">Expert Advice</div>
                 </div>
             </div>
